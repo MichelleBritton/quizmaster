2f2d51dfba65d721746080b6b0f55e35bc4b1ee0
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-07-26 20:38:41 +0000

    Add welcome message and instructions

--- run.py
+++ run.py
@@ -23,4 +23,6 @@ def main():
     Run all program functions
     """
 
+typewriter("Welcome to Quiz Master\nAn app to ensure that your pub quiz runs smoothly\n\n")
+print("Instructions\n\nTo start, enter the amount each person will pay to take part.\nEnter the team names and how many people are in that team.\nSelect a category and how many questions you would like and then all you need to do is read the questions and provide the answers!\nEnter the scores for each team at the end to show the leaderboard")
 main()
