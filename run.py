dc9950f4bab65fcafc33b071508f08b485b6476e
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-07-26 20:17:09 +0000

    Import json, colorama and tabulate modules

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import modules
+import json
+from colorama import Fore, Back, Style
+from tabulate import tabulate
+
+
