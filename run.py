14cd0454ad203e54fd3a78b70a5eb88dfbb4c700
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-07-26 20:37:56 +0000

    Import sys, time and add typewriter function

--- run.py
+++ run.py
@@ -2,7 +2,21 @@
 import json
 from colorama import Fore, Back, Style
 from tabulate import tabulate
+import sys,time
 
+def typewriter(message):
+    """
+    Create typewriter effect
+    Credit: https://learnlearn.uk/python/python-typewriter-code
+    """
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        
+        if char !=  "\n":
+            time.sleep(0.1)
+        else:
+            time.sleep(1)
 
 def main():
     """
