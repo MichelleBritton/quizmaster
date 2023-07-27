069a68f7e677a8efd91cee2010d89c41f1629051
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-07-27 09:26:35 +0000

    Create add_team method

--- run.py
+++ run.py
@@ -46,21 +46,42 @@ class Team:
         self.name = name
         self.size = size
     
-def add_team():
-    """
-    Add Team name and size
-    """
-    print("")
-    name = input("Enter Team Name here: \n") 
-    size = input("Enter size of Team here: \n")
+    def add_team():
+        """
+        Add Team name and size
+        """
+        while True:
+            add_another_team = input("Do you want to add a Team? Y/N \n")      
+
+            if add_another_team.lower() == 'y': 
+                self.name = input("Enter Team Name here: \n") 
+                while True:
+                    try:
+                        self.size = int(input("Enter size of Team here (max. 4 players per team): \n"))
+                        if size > 4 or size == 0:
+                            print("Please enter a number between 1 and 4")
+                        else:
+                            break
+                    except ValueError:
+                        print("Invalid data, please enter a number")
+                    else: 
+                        continue
+                continue
+
+            elif add_another_team.lower() == 'n':
+                break
+            else:
+                print("Please type Y or N")
+        
 
 def main():
     """
     Run all program functions
     """
-    play_amount()
+    #play_amount()
+    Team.add_team()
 
-typewriter("Welcome to Quiz Master\nAn app to ensure that your pub quiz runs smoothly\n\n")
-print("Instructions\n\nTo start, enter the amount each person will pay to take part.\nEnter the team names and how many people are in that team.\nSelect a category and how many questions you would like and then all you need to do is read the questions and provide the answers!\nEnter the scores for each team at the end to show the leaderboard\n")
+#typewriter("Welcome to Quiz Master\nAn app to ensure that your pub quiz runs smoothly\n\n")
+#print("Instructions\n\nTo start, enter the amount each person will pay to take part.\nEnter the team names and how many people are in that team.\nSelect a category and how many questions you would like and then all you need to do is read the questions and provide the answers!\nEnter the scores for each team at the end to show the leaderboard\n")
 
 main()
