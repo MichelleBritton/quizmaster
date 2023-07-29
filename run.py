5e419ad56e9d3f36649be8d490afdf8a46f33147
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-07-29 16:28:08 +0000

    Add colours

--- run.py
+++ run.py
@@ -28,14 +28,14 @@ def play_amount():
     """
     while True:
         print()
-        print("Please enter the play amount per person")    
+        print(Fore.BLUE + "Please enter the play amount per person")    
         try:
-            amount = float(input("Enter the fee here:\n"))
+            amount = float(input(Fore.GREEN + "Enter the fee here:\n"))
         except ValueError:
-            print("Invalid data, please enter a number")
+            print(Fore.RED + "Invalid data, please enter a number")
         else:
             print()
-            print (f"The fee per person is {POUND}{amount:.2f}\n")
+            print (Fore.BLUE + f"The fee per person is {POUND}{amount:.2f}\n")
             break
     return amount
 
@@ -50,14 +50,14 @@ class Team(metaclass=IterTeam):
     _allTeams = []
     def __init__(self):
         self._allTeams.append(self)
-        self.name = input("Enter Team Name here: \n")      
+        self.name = input(Fore.GREEN + "Enter Team Name here: \n")      
         self.size = get_size_from_user()    
     
     def display_teams(self):
         """
         Show the number of people in each team
         """
-        print(f"There are {self.size} people in Team {self.name}")
+        print(Fore.BLUE + f"There are {self.size} people in Team {self.name}")
     
 def get_size_from_user():
     """
@@ -65,13 +65,13 @@ def get_size_from_user():
     """
     while True:
         try:
-            size = int(input("Enter size of Team here (max. 4 players per team): \n"))
+            size = int(input(Fore.GREEN + "Enter size of Team here (max. 4 players per team): \n"))
             if size > 4 or size == 0:
-                print("Please enter a number between 1 and 4")
+                print(Fore.RED + "Please enter a number between 1 and 4")
             else:
                 break
         except ValueError:
-            print("Invalid data, please enter a number")
+            print(Fore.RED + "Invalid data, please enter a number")
         else: 
             continue
     return size
@@ -84,14 +84,14 @@ def add_team():
     teams = []
     while True:
         teams.append(Team())
-        add_another_team = input("\nDo you want to add a Team? Y/N \n")  
+        add_another_team = input(Fore.GREEN + "\nDo you want to add a Team? Y/N \n")  
         print()   
         if add_another_team.lower() == 'y':            
             continue
         elif add_another_team.lower() == 'n':
             break
         else:
-            print("Invalid input. Please type Y or N") 
+            print(Fore.RED + "Invalid input. Please type Y or N") 
             add_team()    
     return teams
 
@@ -104,7 +104,7 @@ def winnings(data, cost):
     total = 0
     for obj in data:
         total += obj.size
-    print(f"There are {total} people playing\n")
+    print(Fore.BLUE + f"There are {total} people playing\n")
     pot = total * cost
     print(f"The total winnings for this game is {POUND}{pot:.2f}")
 
@@ -120,9 +120,9 @@ def get_questions():
         data = json.load(json_file) 
 
     while True:
-        add_cat = input("\nDo you want to add a category? Y/N \n")     
+        add_cat = input(Fore.GREEN + "\nDo you want to add a category? Y/N \n")     
         if add_cat.lower() == 'y':    
-            print("Please select a category from one of the following options:\n")
+            print(Fore.BLUE + "Please select a category from one of the following options:\n")
 
             # Extract the Categories and add them to a list and print them out
             cat_count = 1
@@ -134,7 +134,7 @@ def get_questions():
             # Select a category and the amount of questions for that category
             while True:    
                 try:
-                    category_selection = int(input("Please type the number of the category here: \n")) 
+                    category_selection = int(input(Fore.GREEN + "Please type the number of the category here: \n")) 
                     if category_selection == 1:
                         category_selection = "Food and Drink"
                     elif category_selection == 2:
@@ -152,14 +152,14 @@ def get_questions():
                     elif category_selection == 8:
                         category_selection = "Quick Fire Questions"
                     else:
-                        print("Please type a number between 1 and 8")
+                        print(Fore.RED + "Please type a number between 1 and 8")
                         continue
                 except ValueError:
-                    print("Invalid data, please enter a number")
+                    print(Fore.RED + "Invalid data, please enter a number")
                 else:                
                     break        
 
-            quantity = int(input("Please enter amount of questions here: \n"))
+            quantity = int(input(Fore.GREEN + "Please enter amount of questions here: \n"))
 
             # Retrieve a number of randomised questions for the selected category and convert dictionary into a list 
             result = list(data[category_selection].items())
@@ -182,20 +182,20 @@ def get_questions():
     q_count = 1
     for f in final_questions:
         print()
-        print(f"Question {q_count}: ", f[0])
+        print(Fore.BLUE + f"Question {q_count}: ", f[0])
         q_count += 1
     
     return final_questions
 
 def show_answers(data):
     print()
-    results = input("Type Y when you are ready to share the answers \n")
+    results = input(Fore.GREEN + "Type Y when you are ready to share the answers \n")
     if results.lower() == 'y':    
         q_count = 1
         for f in data:
             print()
-            print(f"Question {q_count}: ", f[0])
-            print(f"Answer {q_count}: ", f[1])
+            print(Fore.BLUE + f"Question {q_count}: ", f[0])
+            print(Fore.MAGENTA + f"Answer {q_count}: ", f[1])
             q_count += 1
     else: 
         show_answers(data)
@@ -214,7 +214,7 @@ def add_scores():
             # Add the team name to the keys list
             keys.append(team.name)
             # Request score and add it to the values list
-            values.append(int(input(f"Enter the score for Team {team.name}\n")))
+            values.append(int(input(Fore.GREEN + f"Enter the score for Team {team.name}:\n")))
 
     # Create a dictionary from both lists  
     # Credit: https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/     
@@ -233,7 +233,7 @@ def show_leaderboard(x):
     """
     print()
     headers = ["Team Name", "Score"]
-    print(tabulate(x.items(), headers = headers))
+    print(Fore.CYAN + tabulate(x.items(), headers = headers))
 
     winner = max(x)
     print()
@@ -253,7 +253,7 @@ def main():
     scores = add_scores()
     show_leaderboard(scores)
 
-typewriter("Welcome to Quiz Master\nAn app to ensure that your pub quiz runs smoothly\n\n")
+typewriter(Fore.BLUE + "Welcome to Quiz Master\nAn app to ensure that your pub quiz runs smoothly\n\n")
 print("Instructions\n\nTo start, enter the team names and how many people are in that team.\nEnter the amount each person will pay to take part.\nSelect a category and how many questions you would like and then all you need to do is read the questions and provide the answers!\nEnter the scores for each team at the end to show the leaderboard\n")
 
 main()
